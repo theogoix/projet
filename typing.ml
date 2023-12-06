1fbdae351c009cbc0fadbc4d5f11e0cb48dbbab5
Theo Goix <theogoix@pop-os.localdomain>
2023-12-06 15:39:38 +0100

    another commit

--- typing.ml
+++ typing.ml
@@ -1,196 +1,321 @@
- 
 
+(* TD 6 Unification et algorithme W -- version avec unification destructive *)
 
-(* Copié collé de ce que j'ai fait au TD6 *)
-
+(* types et variables de types sont définis mutuellement récursivement *)
 
 type typ =
-| Tint
-| Tbool
-| Tstring
-| Tarrow of typ * typ
-| Tproduct of typ * typ (* pas nécéssaire ? *)
-| Tvar of tvar
+  | Tint
+  | Tarrow of typ * typ
+  | Tproduct of typ * typ
+  | Tvar of tvar
 
 and tvar =
-{ id : int;
-  mutable def : typ option } ;;
+  { id : int;
+    mutable def : typ option }
 
-
-(* pretty printer*)
-
-let rec pp_typ fmt = function
-| Tproduct (t1, t2) -> Format.fprintf fmt "%a *@ %a" pp_atom t1 pp_atom t2
-| Tarrow (t1, t2) -> Format.fprintf fmt "%a ->@ %a" pp_atom t1 pp_typ t2
-| _ as t -> pp_atom fmt t
-and pp_atom fmt = function
-| Tint -> Format.fprintf fmt "int"
-| Tbool -> Format.fprintf fmt "bool"
-| Tstring -> Format.fprintf fmt "string"
-| Tvar v -> pp_tvar fmt v
-| Tarrow _ | Tproduct _ as t -> Format.fprintf fmt "@[<1>(%a)@]" pp_typ t
-and pp_tvar fmt = function
-| { def = None; id } -> Format.fprintf fmt "'%d" id
-| { def = Some t; id } -> Format.fprintf fmt "@[<1>('%d := %a)@]" id pp_typ t ;;
-
+(* module V pour les variables de type *)
 
 module V = struct
-type t = tvar
-let compare v1 v2 = Stdlib.compare v1.id v2.id
-let equal v1 v2 = v1.id = v2.id
-let create = let r = ref 0 in fun () -> incr r; { id = !r; def = None }
+  type t = tvar
+  let compare v1 v2 = Stdlib.compare v1.id v2.id
+  let equal v1 v2 = v1.id = v2.id
+  let create = let r = ref 0 in fun () -> incr r; { id = !r; def = None }
 end
 
+(* réduction en tête d'un type (la compression de chemin serait possible) *)
+let rec head = function
+  | Tvar { def = Some t } -> head t
+  | t -> t
 
-(* QUESTION 1 *)
+(* forme canonique d'un type = on applique head récursivement *)
+let rec canon t = match head t with
+  | Tvar _ | Tint as t -> t
+  | Tarrow (t1, t2) -> Tarrow (canon t1, canon t2)
+  | Tproduct (t1, t2) -> Tproduct (canon t1, canon t2)
 
-let rec head t = match t with
-| Tvar({id = _; def = Some t2}) -> head t2
-| _ -> t ;;
+(* tests *)
+let rec pp_typ fmt = function
+  | Tproduct (t1, t2) -> Format.fprintf fmt "%a *@ %a" pp_atom t1 pp_atom t2
+  | Tarrow (t1, t2) -> Format.fprintf fmt "%a ->@ %a" pp_atom t1 pp_typ t2
+  | (Tint | Tvar _) as t -> pp_atom fmt t
+and pp_atom fmt = function
+  | Tint -> Format.fprintf fmt "int"
+  | Tvar v -> pp_tvar fmt v
+  | Tarrow _ | Tproduct _ as t -> Format.fprintf fmt "@[<1>(%a)@]" pp_typ t
+and pp_tvar fmt = function
+  | { def = None; id } -> Format.fprintf fmt "'%d" id
+  | { def = Some t; id } -> Format.fprintf fmt "@[<1>('%d := %a)@]" id pp_typ t
 
-let rec canon t = match t with
-| Tvar({def = Some t2}) -> canon t2
-| Tproduct(t1, t2) -> Tproduct(canon t1, canon t2)
-| Tarrow(t1, t2) -> Tarrow(canon t1, canon t2)
-| _ -> t ;;
+let () =
+  let a = V.create () in
+  let b = V.create () in
+  let ta = Tvar a in
+  let tb = Tvar b in
+  assert (head ta == ta);
+  assert (head tb == tb);
+  let ty = Tarrow (ta, tb) in
+  a.def <- Some tb;
+  assert (head ta == tb);
+  assert (head tb == tb);
+  b.def <- Some Tint;
+  assert (head ta = Tint);
+  assert (head tb = Tint);
+  assert (canon ta = Tint);
+  assert (canon tb = Tint);
+  assert (canon ty = Tarrow (Tint, Tint))
 
+(* unification *)
 
-(* QUESTION 2 *)
+exception UnificationFailure of typ * typ
 
-exception UnificationFailure of typ * typ;;
+let unification_error t1 t2 = raise (UnificationFailure (canon t1, canon t2))
 
-let unification_error t1 t2 = raise (UnificationFailure (canon t1, canon t2));;
+let rec occur v t = match head t with
+  | Tvar w -> V.equal v w
+  | Tarrow (t1, t2) | Tproduct (t1, t2) -> occur v t1 || occur v t2
+  | Tint -> false
 
-let rec occur tv t = match head t with
-| Tint | Tbool | Tstring -> false
-| Tproduct(t1, t2) -> occur tv t1 || occur tv t2
-| Tarrow(t1, t2) -> occur tv t1 || occur tv t2
-| Tvar({id = i}) -> i = tv.id;;
+let rec unify t1 t2 = match head t1, head t2 with
+  | Tint, Tint ->
+      ()
+  | Tvar v1, Tvar v2 when V.equal v1 v2 ->
+      ()
+  | Tvar v1 as t1, t2 ->
+      if occur v1 t2 then unification_error t1 t2;
+      assert (v1.def = None);
+      v1.def <- Some t2
+  | t1, Tvar v2 ->
+      unify t2 t1
+  | Tarrow (t11, t12), Tarrow (t21, t22)
+  | Tproduct (t11, t12), Tproduct (t21, t22) ->
+      unify t11 t21; unify t12 t22
+  | t1, t2 ->
+      unification_error t1 t2
 
-
-let rec unify t1 t2 = match (head t1, head t2) with
-| Tint, Tint -> ()
-| Tbool, Tbool -> ()
-| Tstring, Tstring -> ()
-| Tproduct(t1a, t1b), Tproduct(t2a, t2b) -> unify t1a t2a ; unify t1b t2b
-| Tarrow(t1a, t1b), Tarrow(t2a, t2b) -> unify t1a t2a ; unify t1b t2b
-| Tvar(tv), _ -> tv.def <- Some (head t2)
-| _ , Tvar(tv) -> tv.def <- Some (head t1)
-| _ -> unification_error t1 t2;;
-
+let () =
+  let a = V.create () in
+  let b = V.create () in
+  let ta = Tvar a in
+  let tb = Tvar b in
+  assert (occur a ta);
+  assert (occur b tb);
+  assert (not (occur a tb));
+  let ty = Tarrow (ta, tb) in
+  assert (occur a ty);
+  assert (occur b ty);
+  (* unifie 'a-> 'b et int->int *)
+  unify ty (Tarrow (Tint, Tint));
+  assert (canon ta = Tint);
+  assert (canon ty = Tarrow (Tint, Tint));
+  (* unifie 'c et int->int *)
+  let c = V.create () in
+  let tc = Tvar c in
+  unify tc ty;
+  assert (canon tc = Tarrow (Tint, Tint))
 
 let cant_unify ty1 ty2 =
-try let _ = unify ty1 ty2 in false with UnificationFailure _ -> true;;
+  try let _ = unify ty1 ty2 in false with UnificationFailure _ -> true
 
-(* QUESTION 3 *)
+let () =
+  assert (cant_unify Tint (Tarrow (Tint, Tint)));
+  assert (cant_unify Tint (Tproduct (Tint, Tint)));
+  let a = V.create () in
+  let ta = Tvar a in
+  unify ta (Tarrow (Tint, Tint));
+  assert (cant_unify ta Tint)
+
+(* schéma de type *)
 
 module Vset = Set.Make(V)
 
+type schema = { vars : Vset.t; typ : typ }
+
+(* variables libres *)
+
 let rec fvars t = match head t with
-| Tint | Tbool | Tstring -> Vset.empty
-| Tproduct(t1, t2) -> Vset.union (fvars t1) (fvars t2)
-| Tarrow(t1, t2) -> Vset.union (fvars t1) (fvars t2) 
-| Tvar(v) -> Vset.singleton v ;;
+  | Tint -> Vset.empty
+  | Tarrow (t1, t2) | Tproduct (t1, t2) -> Vset.union (fvars t1) (fvars t2)
+  | Tvar v -> Vset.singleton v
 
+let norm_varset s =
+  Vset.fold (fun v s -> Vset.union (fvars (Tvar v)) s) s Vset.empty
 
-(* QUESTION 4 *)
+let () =
+  assert (Vset.is_empty (fvars (Tarrow (Tint, Tint))));
+  let a = V.create () in
+  let ta = Tvar a in
+  let ty = Tarrow (ta, ta) in
+  assert (Vset.equal (fvars ty) (Vset.singleton a));
+  unify ty (Tarrow (Tint, Tint));
+  assert (Vset.is_empty (fvars ty))
 
-type schema = { vars : Vset.t; typ : typ };;
+(* environnement c'est une table bindings (string -> schema),
+   et un ensemble de variables de types libres *)
 
-module Smap = Map.Make(String);;
+module Smap = Map.Make(String)
 
-type env = { bindings : schema Smap.t; fvars : Vset.t };;
+type env = { bindings : schema Smap.t; fvars : Vset.t }
 
-let empty = {bindings = Smap.empty ; fvars = Vset.empty};;
+let empty = { bindings = Smap.empty; fvars = Vset.empty }
 
-
-let add str t env = {bindings = Smap.(env.bindings |> add str { vars = Vset.empty; typ = t }) ; fvars = Vset.union env.fvars (fvars t)};;
-
-let add_gen str t env = {bindings = Smap.(env.bindings |> add str { vars = Vset.diff (fvars t) env.fvars; typ = t }) ; fvars = env.fvars};;
+let add gen x t env =
+  let vt = fvars t in
+  let s, fvars =
+    if gen then
+      let env_fvars = norm_varset env.fvars in
+      { vars = Vset.diff vt env_fvars; typ = t }, env.fvars
+    else
+      { vars = Vset.empty; typ = t }, Vset.union env.fvars vt
+  in
+  { bindings = Smap.add x s env.bindings; fvars = fvars }
 
 module Vmap = Map.Make(V)
 
-(* renoie un Vmap de tvar qui a chaque variable de type libre dans
-   le schéma associe une variable fraiche *)
-let rec substitutions vlist = 
-match vlist with
-  | [] -> Vmap.empty
-  | a :: q -> let subs = substitutions q in Vmap.(subs |> add a (V.create ()))
+(* find x env donne une instance fraîche de env(x) *)
+let find x env =
+  let tx = Smap.find x env.bindings in
+  let s =
+    Vset.fold (fun v s -> Vmap.add v (Tvar (V.create ())) s)
+      tx.vars Vmap.empty
+  in
+  let rec subst t = match head t with
+    | Tvar x as t -> (try Vmap.find x s with Not_found -> t)
+    | Tint -> Tint
+    | Tarrow (t1, t2) -> Tarrow (subst t1, subst t2)
+    | Tproduct (t1, t2) -> Tproduct (subst t1, subst t2)
+  in
+  subst tx.typ
 
-(* avec le Vmap précédent, remplace toutes les occurences par les
-   variables fraiches dans t *)
-let rec substitute subs t = match head t with
-  | Tint | Tbool | Tstring -> t
-  | Tproduct(t1, t2) -> Tproduct(substitute subs t1, substitute subs t2)
-  | Tarrow(t1, t2) -> Tarrow(substitute subs t1, substitute subs t2)
-  | Tvar(tv) -> let replacement = Vmap.find_opt tv subs in
-                match replacement with
-                  | None -> t
-                  | Some tv2 -> Tvar(tv2)
+(* syntaxe abstraite des programmes *)
+type expression =
+  | Var of string
+  | Const of int
+  | Op of string
+  | Fun of string * expression
+  | App of expression * expression
+  | Pair of expression * expression
+  | Let of string * expression * expression
 
-
-let find str env =
-let sigma = Smap.find str env.bindings in
-substitute (substitutions (Vset.elements sigma.vars)) sigma.typ
-
-
-(* TODO : question 5 du TD en gros *)
-
-open Ast
-
+(* algorithme W *)
 let rec w env = function
-  | Evar x ->
+  | Var x ->
       find x env
-  | Ecst (Cint _) ->
+  | Const _ ->
       Tint
-  | Ecst (Cbool _) ->
-      Tbool
-  | Ecst (Cstring _) ->
-      Tstring
-  | Ebinop (op, e1, e2) ->
-    begin match op with
-      | Add | Sub | Mul | Div ->
-        let t1 = w env e1 in
-        let t2 = w env e2 in
-        unify t1 Tint;
-        unify t2 Tint;
-        Tint
-      | Gt | Ge | Lt | Le ->
-          let t1 = w env e1 in
-          let t2 = w env e2 in
-          unify t1 Tint;
-          unify t2 Tint;
-          Tbool
-      | Conc ->
-          let t1 = w env e1 in
-          let t2 = w env e2 in
-          unify t1 Tstring;
-          unify t2 Tstring;
-          Tstring
-      | And | Or ->
-          let t1 = w env e1 in
-          let t2 = w env e2 in
-          unify t1 Tbool;
-          unify t2 Tbool;
-          Tbool
-      | _ -> failwith "missing binop in W"
-    end
-(*  | Fun (x, e1) ->
+  | Op "+" ->
+      Tarrow (Tproduct (Tint, Tint), Tint)
+  | Op op ->
+      failwith ("pas d'opérateur " ^ op)
+  | Pair (e1, e2) ->
+      let t1 = w env e1 in
+      let t2 = w env e2 in
+      Tproduct (t1, t2)
+  | Fun (x, e1) ->
       let v = Tvar (V.create ()) in
       let env = add false x v env in
       let t1 = w env e1 in
-      Tarrow (v, t1)*)
-(*  | App (e1, e2) ->
+      Tarrow (v, t1)
+  | App (e1, e2) ->
       let t1 = w env e1 in
       let t2 = w env e2 in
       let v = Tvar (V.create ()) in
       unify t1 (Tarrow (t2, v));
-      v*)
-  | Elet(li, e) ->
-      w (List.fold_left add_binding_gen env li) e
-  | _ -> failwith "missing case in W"
+      v
+  | Let (x, e1, e2) ->
+      let t1 = w env e1 in
+      let env = add true x t1 env in
+      w env e2
 
-and add_binding_gen env bind = 
-  let x, e = bind in
-  let t = w env e in
-  add_gen x t env
+
+
+(* tests *)
+
+let typeof e = canon (w empty e)
+
+(* positifs *)
+
+(* 1 : int *)
+let () = assert (typeof (Const 1) = Tint)
+
+(* fun x -> x : 'a -> 'a *)
+let () = assert (match typeof (Fun ("x", Var "x")) with
+  | Tarrow (Tvar v1, Tvar v2) -> V.equal v1 v2
+  | _ -> false)
+
+(* fun x -> x+1 : int -> int *)
+let () = assert (typeof (Fun ("x", App (Op "+", Pair (Var "x", Const 1))))
+                 = Tarrow (Tint, Tint))
+
+(* fun x -> x+x : int -> int *)
+let () = assert (typeof (Fun ("x", App (Op "+", Pair (Var "x", Var "x"))))
+                 = Tarrow (Tint, Tint))
+
+(* let x = 1 in x+x : int *)
+let () =
+  assert (typeof (Let ("x", Const 1, App (Op "+", Pair (Var "x", Var "x"))))
+          = Tint)
+
+(* let id = fun x -> x in id 1 *)
+let () =
+  assert (typeof (Let ("id", Fun ("x", Var "x"), App (Var "id", Const 1)))
+          = Tint)
+
+(* let id = fun x -> x in id id 1 *)
+let () =
+  assert (typeof (Let ("id", Fun ("x", Var "x"),
+                       App (App (Var "id", Var "id"), Const 1)))
+          = Tint)
+
+(* let id = fun x -> x in (id 1, id (1,2)) : int * (int * int) *)
+let () =
+  let ty = typeof (Let ("id", Fun ("x", Var "x"),
+                        Pair (App (Var "id", Const 1),
+                              App (Var "id", Pair (Const 1, Const 2))))) in
+  assert (ty
+          = Tproduct (Tint, Tproduct (Tint, Tint)))
+
+(* app = fun f x -> let y = f x in y : ('a -> 'b) -> 'a -> 'b *)
+let () =
+  let ty =
+    typeof (Fun ("f", Fun ("x", Let ("y", App (Var "f", Var "x"), Var "y"))))
+  in
+  assert (match ty with
+    | Tarrow (Tarrow (Tvar v1, Tvar v2), Tarrow (Tvar v3, Tvar v4)) ->
+        V.equal v1 v3 && V.equal v2 v4
+    | _ -> false)
+
+(* négatifs *)
+
+let cant_type e =
+  try let _ = typeof e in false with UnificationFailure _ -> true
+
+(* 1 2 *)
+let () = assert (cant_type (App (Const 1, Const 2)))
+
+(* fun x -> x x *)
+let () = assert (cant_type (Fun ("x", App (Var "x", Var "x"))))
+
+(* (fun f -> +(f 1)) (fun x -> x) *)
+let () = assert (cant_type
+                   (App (Fun ("f", App (Op "+", App (Var "f", Const 1))),
+                         Fun ("x", Var "x"))))
+
+(* fun x -> (x 1, x (1,2)) *)
+let () = assert (cant_type
+                   (Fun ("x", Pair (App (Var "x", Const 1),
+                                    App (Var "x", Pair (Const 1, Const 2))))))
+
+(* fun x -> let z = x in (z 1, z (1,2)) *)
+let () = assert (cant_type
+                   (Fun ("x",
+                         Let ("z", Var "x",
+                              Pair (App (Var "z", Const 1),
+                                    App (Var "z", Pair (Const 1, Const 2)))))))
+
+(* let distr_pair = fun f -> (f 1, f (1,2)) in distr_pair (fun x -> x) *)
+let () =
+  assert (cant_type
+            (Let ("distr_pair",
+                  Fun ("f", Pair (App (Var "f", Const 1),
+                                  App (Var "f", Pair (Const 1, Const 2)))),
+                  App (Var "distr_pair", (Fun ("x", Var "x"))))))
