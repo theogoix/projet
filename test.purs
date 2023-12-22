24887323d9773cf72199d937cd04a984e220ad73
Octave Mortain <omortain@clipper.ens.fr>
2023-12-22 12:18:27 +0100

    Fonctions en case, déclaration des classes

--- test.purs
+++ test.purs
@@ -3,5 +3,14 @@ import Prelude
 import Effect
 import Effect.Console
 
-main::Unit->Unit
-main = if 5 then mod 5 4 else 2 
+class C a where
+  foo:: a -> String
+  
+main :: Effect Unit
+main = log (foo 1)
+
+main2 :: Effect Unit
+main2 = log (foo "aa")
+
+g:: Int
+g = foo
